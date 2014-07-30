59e81409a4658d3f2294ce2931274054426ba67e
polwath <polwath_7@hotmail.com>
2014-07-30 13:54:56 +0900

    Updates

---
+++ XMLParameter.rb
@@ -0,0 +1,80 @@
+class XMLParameter
+  def initialize
+    @keylist = Array.new {Hash.new}
+    @valuelist = Array.new {Hash.new}
+    @pointer = 0
+  end
+  
+  def add(key, value)
+    @keylist.push(key)
+    @valuelist.push(value)
+  end
+  
+  def pop
+    begin
+      valueString = Array.new
+      valueString[0] = @keylist[@pointer]
+      valueString[1] = @valuelist[@pointer]
+      @pointer += 1
+      return valueString
+    rescue
+      puts "Index out of bound."
+    end
+    return nil
+  end
+  
+  def peek
+    begin
+      valueString = Array.new
+      valueString[0] = @keylist[@pointer]
+      valueString[1] = @valuelist[@pointer]
+      return valueString
+    rescue
+      puts "Index out of bound."
+    end
+    return nil
+  end
+  
+  def hasNext
+    return @pointer < keylist.count
+  end
+  
+  def first
+    @pointer = 0
+  end
+  
+  def last
+    @pointer = keylist.count
+  end
+  
+  def getValue(key)
+    p = 0
+    while p < @keylist.count do
+      if @keylist[p] == key
+        puts @valuelist[p];
+      else
+        p += 1
+      end
+    end
+    return nil
+  end
+  
+  def clear
+    @keylist = Array.new
+    @valuelist = Array.new
+    first
+    return first
+  end
+end
+
+value = XMLParameter.new
+value.add("Serial",1234567)
+puts value.pop
+value.add("Domain","Polwath.com")
+puts value.pop
+value.add("Name","Polwath")
+puts value.pop
+value.add("Surname","Rojchalermritra")
+puts value.pop
+value.add("username","polwath2534")
+puts value.pop
