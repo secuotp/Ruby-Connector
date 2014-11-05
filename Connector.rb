f609ef0308725065ccb16adc0e869bb919634e2b
polwath <polwath_7@hotmail.com>
2014-11-05 12:51:59 +0900

    Repair commit

--- Connector.rb
+++ Connector.rb
@@ -4,33 +4,33 @@ require_relative "XMLParameter.rb"
 require 'net/http'
 
 req = XMLRequest.new
-#req.setSid("M-01")
-req.setSid("U-02")
+req.setSid("M-01")
+#req.setSid("U-02")
 #req.setSid("U-01")
 req.setDomainName("https://hoax.co.jp")
 req.setSerialNumber("34BWS-GH56N-JQ45M-PP2HG")
 req.setParamTag(Array.new {Hash.new})
-#req.addChildValue("username", "WTF")
-#req.addChildValue("email", "admin@wtf.com");
-#req.addChildValue("fname", "TheHeck");
-#req.addChildValue("lname", "NoYOLO");
-#req.addChildValue("phone", "1819998233");
+req.addChildValue("username", "WTF")
+req.addChildValue("email", "admin@wtf.com");
+req.addChildValue("fname", "TheHeck");
+req.addChildValue("lname", "NoYOLO");
+req.addChildValue("phone", "1819998233");
 
-req.addChildValue("username", "Dafuq")
+#req.addChildValue("username", "Dafuq")
 #req.addChildValue("type", "full");
-changeTag = req.addChildTag("change")
-changeTag.setChildNode(Array.new {Hash.new})
-changeTag.addChildValue("param", "email")
-changeTag.addChildValue("value", "yolo@xyz.com")
+#changeTag = req.addChildTag("change")
+#changeTag.setChildNode(Array.new {Hash.new})
+#changeTag.addChildValue("param", "email")
+#changeTag.addChildValue("value", "yolo@xyz.com")
 
 service = ServiceCode.new
 xml = req.toString
 dataSize = xml.bytesize.to_s
-#uri = URI.parse(service.getServiceUri("U-01"))
-uri = URI.parse(service.getServiceUri("U-02"))
+uri = URI.parse(service.getServiceUri("U-01"))
+#uri = URI.parse(service.getServiceUri("U-02"))
 #uri = URI.parse(service.getServiceUri("M-01"))
-request = Net::HTTP::Put.new(uri.path)
-#request = Net::HTTP::Post.new(uri.path)
+#request = Net::HTTP::Put.new(uri.path)
+request = Net::HTTP::Post.new(uri.path)
 request.content_length = dataSize
 request.content_type = "application/xml"
 request.body = "request="+xml
@@ -42,7 +42,7 @@ param = res.getParameter
 
 puts response.body
 
-while param.hasNext
-  a = param.pop
-  puts "#{a[0]}\t#{a[1]}"
-end
+#while param.hasNext
+#  a = param.pop
+#  puts "#{a[0]}\t#{a[1]}"
+#end
